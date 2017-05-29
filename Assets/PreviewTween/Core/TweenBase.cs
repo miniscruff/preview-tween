8e391d81e6823d06edd9775e02d1909862951d03
Ronnie <halfpint1170@gmail.com>
2017-05-28 22:17:48 -0700

    First draft of tween base, still got a few more features before its gonna be ready

---
+++ Assets/PreviewTween/Core/TweenBase.cs
@@ -0,0 +1,69 @@
+﻿namespace PreviewTween
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public abstract class TweenBase<T> : MonoBehaviour
+    {
+        private T _start;
+        private T _end;
+        private T _value;
+        private float _progress;
+        private float _duration = 1f;
+
+        public T start
+        {
+            get { return _start; }
+            set { _start = value; }
+        }
+
+        public T end
+        {
+            get { return _end; }
+            set { _end = value; }
+        }
+
+        public T value
+        {
+            get { return _value; }
+        }
+
+        public float duration
+        {
+            get { return _duration; }
+            set { _duration = value; }
+        }
+
+        public float progress
+        {
+            get { return _progress; }
+            set { _progress = value; }
+        }
+
+        public void Sample()
+        {
+            _value = UpdateValue(_progress);
+        }
+
+        public void Play()
+        {
+            StartCoroutine(RunTween());
+        }
+
+        private IEnumerator RunTween()
+        {
+            // sample before starting our loop as well
+            Sample();
+
+            while (_progress < 1f)
+            {
+                yield return null;
+
+                _progress = Mathf.Clamp01(_progress + Time.deltaTime / _duration);
+                Sample();
+            }
+        }
+
+        protected abstract T UpdateValue(float time);
+    }
+}
---
+++ Assets/Tests/PlayMode/TweenBaseTests.cs
@@ -0,0 +1,102 @@
+﻿namespace PreviewTween
+{
+    using System.Collections;
+    using NUnit.Framework;
+    using UnityEngine;
+    using UnityEngine.TestTools;
+
+    public sealed class TestTween : TweenBase<float>
+    {
+        protected override float UpdateValue(float time)
+        {
+            return Mathf.Lerp(start, end, time);
+        }
+    }
+
+    public sealed class TweenBaseTests
+    {
+        private GameObject _tweenObject;
+        private TestTween _tween;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tweenObject = new GameObject("Tween");
+            _tween = _tweenObject.AddComponent<TestTween>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(_tweenObject);
+        }
+
+        [Test]
+        public void Sample_ProgressZero_HasStartValue()
+        {
+            _tween.start = 10f;
+            _tween.end = 25f;
+            _tween.Sample();
+
+            Assert.AreEqual(10f, _tween.value);
+        }
+
+        [Test]
+        public void Sample_ProgressHalf_HasMiddleValue()
+        {
+            _tween.start = 10f;
+            _tween.end = 20f;
+            _tween.progress = 0.5f;
+            _tween.Sample();
+
+            Assert.AreEqual(15f, _tween.value);
+        }
+
+        [Test]
+        public void Sample_ProgressOne_HasEndValue()
+        {
+            _tween.start = 10f;
+            _tween.end = 25f;
+            _tween.progress = 1f;
+            _tween.Sample();
+
+            Assert.AreEqual(25f, _tween.value);
+        }
+
+        [Test]
+        public void Play_ImmediatelyHasStartValue()
+        {
+            _tween.start = 10f;
+            _tween.end = 20f;
+
+            _tween.Play();
+
+            Assert.AreEqual(10f, _tween.value);
+        }
+
+        [UnityTest]
+        public IEnumerator Play_DefaultDuration_SetsValueToEnd()
+        {
+            _tween.start = 10f;
+            _tween.end = 20f;
+
+            _tween.Play();
+            yield return new WaitForSeconds(1f);
+
+            Assert.AreEqual(20f, _tween.value);
+        }
+
+        [UnityTest]
+        public IEnumerator PlayTwoSecondDuration_SetsValueToEnd()
+        {
+            _tween.start = 10f;
+            _tween.end = 20f;
+            _tween.duration = 2f;
+
+            _tween.Play();
+            yield return new WaitForSeconds(2f);
+
+            Assert.AreEqual(20f, _tween.value);
+        }
+    }
+}
