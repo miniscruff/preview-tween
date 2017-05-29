f4a3d8147edbb6c82109ab894df4cf7dee20e16d
Ronnie <halfpint1170@gmail.com>
2017-05-29 14:56:49 -0700

    Added tests for tween position, going to make a base class to easily test new tweens. The only thing you need to test for each new tween is whether or not applying at any given progress in linear mode yields the correct result. All other tests are handled by the generic base test system.

---
+++ Assets/Tests/Editor/Tweens/TweenPositionTests.cs
@@ -0,0 +1,53 @@
+﻿namespace PreviewTween
+{
+    using NUnit.Framework;
+    using UnityEngine;
+
+    public sealed class TweenPositionTests
+    {
+        private TweenPosition _tweenPosition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            GameObject go = new GameObject("TweenPosition");
+            _tweenPosition = go.AddComponent<TweenPosition>();
+
+            _tweenPosition.start = new Vector3(10, 0, 0);
+            _tweenPosition.end = new Vector3(20, 0, 0);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(_tweenPosition.gameObject);
+        }
+
+        [Test]
+        public void ValueAtStart()
+        {
+            _tweenPosition.progress = 0f;
+            _tweenPosition.Apply();
+
+            Assert.AreEqual(new Vector3(10, 0, 0), _tweenPosition.transform.position);
+        }
+
+        [Test]
+        public void ValueAtEnd()
+        {
+            _tweenPosition.progress = 1f;
+            _tweenPosition.Apply();
+
+            Assert.AreEqual(new Vector3(20, 0, 0), _tweenPosition.transform.position);
+        }
+
+        [Test]
+        public void ValueAtMiddle()
+        {
+            _tweenPosition.progress = 0.5f;
+            _tweenPosition.Apply();
+
+            Assert.AreEqual(new Vector3(15, 0, 0), _tweenPosition.transform.position);
+        }
+    }
+}
