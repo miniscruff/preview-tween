bad3ba6d60561004c886060fbc2f80b8e7dbfed3
Ronnie <halfpint1170@gmail.com>
2017-06-09 01:03:31 -0700

    Renamed basic cubes scene so it wasnt numbered Created a very simple menu scene so it can be auto built from unity cloud and used before downloading

---
+++ Assets/PreviewTween/Samples/Scripts/ButtonLoadScene.cs
@@ -0,0 +1,20 @@
+﻿namespace PreviewTween.Samples
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(Button))]
+    public sealed class ButtonLoadScene : MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+
+        private void Start()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene(_sceneName);
+            });
+        }
+    }
+}
