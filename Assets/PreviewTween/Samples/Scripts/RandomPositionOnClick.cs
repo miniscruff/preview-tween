bcc7953f0c1d3f476f849f974fcdf450a745a162
Ronnie <halfpint1170@gmail.com>
2017-06-09 23:20:08 -0700

    Created a random position samples showing how you can change the data at runtime

---
+++ Assets/PreviewTween/Samples/Scripts/RandomPositionOnClick.cs
@@ -0,0 +1,26 @@
+﻿namespace PreviewTween.Samples
+{
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    /// <summary>
+    /// Simple demo showing how you can use external scripts to control a tween during runtime
+    /// </summary>
+    public sealed class RandomPositionOnClick : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private TweenPosition _tween;
+        [SerializeField] private Vector3 _boundsMin;
+        [SerializeField] private Vector3 _boundsMax;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _tween.start = _tween.target.position;
+            _tween.end = new Vector3(
+                Random.Range(_boundsMin.x, _boundsMax.x),
+                Random.Range(_boundsMin.y, _boundsMax.y),
+                Random.Range(_boundsMin.z, _boundsMax.z)
+            );
+            _tween.Replay();
+        }
+    }
+}
