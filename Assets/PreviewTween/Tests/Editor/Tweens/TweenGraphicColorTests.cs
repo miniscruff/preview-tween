6389314413c8b4ce6e4b04c829ec80be41b15079
Ronnie <halfpint1170@gmail.com>
2017-07-04 19:09:40 -0700

    Tween graphics color is now ready

---
+++ Assets/PreviewTween/Tests/Editor/Tweens/TweenGraphicColorTests.cs
@@ -0,0 +1,126 @@
+﻿namespace PreviewTween
+{
+    using NUnit.Framework;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public sealed class TweenGraphicColorTests : SetupTweenTests<TweenGraphicColor>
+    {
+        public override void SetUp()
+        {
+            GameObject go = new GameObject("Tween");
+            // use Image as our graphic as graphic is abstract
+            Graphic graphic = go.AddComponent<Image>();
+
+            SetUp(go);
+
+            tween.gradient = new Gradient
+            {
+                colorKeys = new [] {
+                    new GradientColorKey(Color.white, 0f),
+                    new GradientColorKey(Color.black, 1f)
+                },
+                alphaKeys = new[] {
+                    new GradientAlphaKey(0f, 0f),
+                    new GradientAlphaKey(1f, 1f)
+                },
+                mode = GradientMode.Blend
+            };
+            tween.progress = 0.5f;
+
+            Assert.AreEqual(graphic, tween.target);
+        }
+
+        [Test]
+        public void SetsColor()
+        {
+            tween.Apply();
+            Assert.AreEqual(new Color(0.5f, 0.5f, 0.5f, 0.5f), tween.target.color);
+        }
+
+        [Test]
+        public void SeparateTarget()
+        {
+            GameObject target = new GameObject("Target");
+            Graphic graphic = target.AddComponent<Image>();
+
+            tween.target = graphic;
+            tween.Apply();
+
+            Assert.AreEqual(new Color(0.5f, 0.5f, 0.5f, 0.5f), graphic.color);
+
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void RecordStart()
+        {
+            tween.target.color = Color.blue;
+            tween.RecordStart();
+
+            Assert.AreEqual(Color.blue, tween.gradient.Evaluate(0f));
+        }
+
+        [Test]
+        public void RecordEnd()
+        {
+            tween.target.color = Color.red;
+            tween.RecordEnd();
+
+            Assert.AreEqual(Color.red, tween.gradient.Evaluate(1f));
+        }
+
+        [Test]
+        public void RecordStart_WithTarget()
+        {
+            GameObject target = new GameObject("Target");
+            Graphic graphic = target.AddComponent<Image>();
+            graphic.color = Color.green;
+
+            tween.target = graphic;
+            tween.RecordStart();
+
+            Assert.AreEqual(Color.green, tween.gradient.Evaluate(0f));
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void RecordEnd_WithTarget()
+        {
+            GameObject target = new GameObject("Target");
+            Graphic graphic = target.AddComponent<Image>();
+            graphic.color = Color.white;
+
+            tween.target = graphic;
+            tween.RecordEnd();
+
+            Assert.AreEqual(Color.white, tween.gradient.Evaluate(1f));
+            Object.DestroyImmediate(target);
+        }
+
+        [Test]
+        public void ResetSetsStartAndEndToCurrentValue()
+        {
+            Graphic graphic = tween.target;
+            Object.DestroyImmediate(tween);
+
+            graphic.color = Color.magenta;
+            TweenGraphicColor tempTween = gameObject.AddComponent<TweenGraphicColor>();
+
+            Assert.AreEqual(Color.magenta, tempTween.gradient.Evaluate(0f));
+            Assert.AreEqual(Color.magenta, tempTween.gradient.Evaluate(1f));
+        }
+
+        [Test]
+        public void ResetWithNoConnectedCanvasGroup_UsesWhiteAndBlackForDefault()
+        {
+            Object.DestroyImmediate(tween.target);
+            Object.DestroyImmediate(tween);
+
+            TweenGraphicColor tempTween = gameObject.AddComponent<TweenGraphicColor>();
+
+            Assert.AreEqual(Color.white, tempTween.gradient.Evaluate(0f));
+            Assert.AreEqual(Color.black, tempTween.gradient.Evaluate(1f));
+        }
+    }
+}
---
+++ Assets/PreviewTween/Tweens/TweenGraphicColor.cs
@@ -0,0 +1,78 @@
+﻿namespace PreviewTween
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public sealed class TweenGraphicColor : TweenBase
+    {
+        [SerializeField] private Graphic _target;
+        [SerializeField] private Gradient _gradient;
+
+        public Graphic target
+        {
+            get { return _target; }
+            set { _target = value; }
+        }
+
+        public Gradient gradient
+        {
+            get { return _gradient; }
+            set { _gradient = value; }
+        }
+
+        private void Reset()
+        {
+            _target = GetComponent<Graphic>();
+
+            GradientColorKey[] colorKeys = new GradientColorKey[2];
+            if (_target != null)
+            {
+                colorKeys[0] = new GradientColorKey(_target.color, 0f);
+                colorKeys[1] = new GradientColorKey(_target.color, 1f);
+            }
+            else
+            {
+                colorKeys[0] = new GradientColorKey(Color.white, 0f);
+                colorKeys[1] = new GradientColorKey(Color.black, 1f);
+            }
+
+            GradientAlphaKey[] alphaKeys = {
+                new GradientAlphaKey(1f, 0f),
+                new GradientAlphaKey(1f, 1f)
+            };
+
+            _gradient = new Gradient {
+                colorKeys = colorKeys,
+                alphaKeys = alphaKeys,
+                mode = GradientMode.Blend
+            };
+        }
+
+        public override void RecordStart()
+        {
+            GradientColorKey[] colorKeys = _gradient.colorKeys;
+            colorKeys[0].color = _target.color;
+
+            GradientAlphaKey[] alphaKeys = _gradient.alphaKeys;
+            alphaKeys[0].alpha = _target.color.a;
+
+            _gradient.SetKeys(colorKeys, alphaKeys);
+        }
+
+        public override void RecordEnd()
+        {
+            GradientColorKey[] colorKeys = _gradient.colorKeys;
+            colorKeys[colorKeys.Length - 1].color = _target.color;
+
+            GradientAlphaKey[] alphaKeys = _gradient.alphaKeys;
+            alphaKeys[alphaKeys.Length - 1].alpha = _target.color.a;
+
+            _gradient.SetKeys(colorKeys, alphaKeys);
+        }
+
+        protected override void UpdateValue(float smoothTime)
+        {
+            _target.color = _gradient.Evaluate(smoothTime);
+        }
+    }
+}
